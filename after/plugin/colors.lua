6b005c0c41706a863680a24b9f1daac0cd39bea3
DorianDevp <dorian.donoch@gmail.com>
2024-02-22 12:29:11 +0100

    added plugins

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,6 +4,9 @@ function ColorMyTheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFLoat", { bg = "none" })
+
+    vim.cmd('highlight RedVariable guifg=#FF0000') -- Ustawienie koloru czerwonego dla zmiennyc
+    vim.cmd('autocmd Syntax * highlight RedVariable guifg=#FF0000') -- Dodać podświetlenie dla każdej składni
 end
 
 ColorMyTheme()
--- lua/dorianpog/packer.lua
+++ lua/dorianpog/packer.lua
@@ -2,42 +2,64 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
- 
-  use ({ 
-	"catppuccin/nvim", as = "catppuccin",
-	config = function()
-		vim.cmd('colorscheme catppuccin')
-	end
-  })
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use ({ 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} })
-  use ({ 'nvim-treesitter/playground' })
-  use ({ 'theprimeagen/harpoon' })
-  use ({ 'mbbill/undotree' })
-  use ({ 'tpope/vim-fugitive' })
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
-		   {'williamboman/mason.nvim'},
-		   {'williamboman/mason-lspconfig.nvim'},
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'L3MON4D3/LuaSnip'},
-	  }
-  }
-  use("lukas-reineke/indent-blankline.nvim")
+    use ({ 
+        "catppuccin/nvim", as = "catppuccin",
+        config = function()
+            vim.cmd('colorscheme catppuccin')
+        end
+    })
+
+    use ({ 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} })
+
+    use ({ 'nvim-treesitter/playground' })
+
+    use ({ 'theprimeagen/harpoon' })
+
+    use ({ 'mbbill/undotree' })
+
+    use ({ 'tpope/vim-fugitive' })
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment the two plugins below if you want to manage the language servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
+
+    use("lukas-reineke/indent-blankline.nvim")
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
+    use({
+        "Pocco81/auto-save.nvim",
+        config = function()
+            require("auto-save").setup {
+                -- your config goes here
+                -- or just leave it empty :)
+            }
+        end,
+    })
+
+    use({ "norcalli/nvim-colorizer.lua" })
 end)
